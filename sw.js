b34d6345a7bd9e8d77283c9baa211bc3e83ad7db
Pablo <phpaez@gmail.com>
2024-03-21 15:38:31 -0300

    css

--- sw.js
+++ sw.js
@@ -13,6 +13,7 @@ const APP_SHELL = [
     'assets/apple-touch-icon.png',
     'assets/favicon-32x32.png',
     'assets/favicon-16x16.png',
+    'assets/logo-loteria.jpg',
     'js/app.js',
     'js/sw-utils.js'
 ];
@@ -29,71 +30,38 @@ const APP_SHELL_INMUTABLE = [
 
 
 self.addEventListener('install', e => {
-
-
     const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
         cache.addAll( APP_SHELL ));
-
     const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
         cache.addAll( APP_SHELL_INMUTABLE ));
-
-
-
     e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
-
 });
 
 
 self.addEventListener('activate', e => {
-
     const respuesta = caches.keys().then( keys => {
-
         keys.forEach( key => {
-
             if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
-
             if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
-
         });
-
     });
-
     e.waitUntil( respuesta );
-
 });
-
-
 
 
 self.addEventListener( 'fetch', e => {
-
-
     const respuesta = caches.match( e.request ).then( res => {
-
         if ( res ) {
             return res;
         } else {
-
             return fetch( e.request ).then( newRes => {
-
                 return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
-
             });
-
         }
-
     });
-
-
-
     e.respondWith( respuesta );
-
 });
-
-
-
-
