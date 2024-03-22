2dde6a3fa93161acc6b0da82fa505b425cd2c014
phpaez <phpaez@gmail.com>
2024-03-22 11:49:20 -0300

    sw

--- sw.js
+++ sw.js
@@ -26,7 +26,9 @@ const APP_SHELL_INMUTABLE = [
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
     'jquery-3.7.1/jquery-3.7.1.min.js',
-    'sweetalert2@11/sweetalert2@11.js'
+    'sweetalert2@11/sweetalert2@11.js',
+    'librerias/twbs-pagination-master/jquery.twbsPagination.min.js',
+    'librerias/fontawesome-free-6.4.2-web/js/all.min.js'
 ];
 
 
