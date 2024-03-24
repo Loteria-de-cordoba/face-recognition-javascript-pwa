8476ff5ce37de7be0ec54872969851369cbf63c7
phpaez <phpaez@gmail.com>
2024-03-24 11:20:12 -0300

    sw

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('sw.js').then(
-        () => {
-            console.log('[SW] Service worker has been registered');
-            push_updateSubscription();
-        },
-        e => {
-            console.error('[SW] Service worker registration failed', e);
-            changePushButtonState('incompatible');
-        }
-    );
+if ( navigator.serviceWorker ) {
+    navigator.serviceWorker.register('sw.js');
 }
