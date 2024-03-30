a839275a0f6d4cec8be2f8cfd8443a77ef04a44a
phpaez <phpaez@gmail.com>
2024-03-30 18:54:49 -0300

    titulo push

--- sw.js
+++ sw.js
@@ -78,7 +78,7 @@ self.addEventListener('push', function (event) {
     const sendNotification = body => {
         // you could refresh a notification badge here with postMessage API
         // const title = "Web Push example";
-        const title = "Face recognition";
+        const title = "Face recognition server";
 
         return self.registration.showNotification(title, {
             body,
