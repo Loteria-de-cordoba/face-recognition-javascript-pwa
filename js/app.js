e63e9477dcc81ff90301df2d13a31bc786a49b26
phpaez <phpaez@gmail.com>
2024-04-22 10:08:10 -0300

    desregistramos sw

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-if ( navigator.serviceWorker ) {
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+        for (const registration of registrations) {
+            registration.unregister();
+        }
+    });
     navigator.serviceWorker.register('sw.js');
 }
+
+
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker
+//       .register("/sw.js", { scope: "/" })
+//       .then((registration) => {
+//         // registration worked
+//         console.log("Registration succeeded.");
+//         registration.unregister().then((boolean) => {
+//           // if boolean = true, unregister is successful
+//         });
+//       })
+//       .catch((error) => {
+//         // registration failed
+//         console.error(`Registration failed with ${error}`);
+//       });
+//   }
