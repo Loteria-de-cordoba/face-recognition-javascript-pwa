5ee3c18aca3ea2339dca6a0498eeb99c57cb8956
phpaez <phpaez@gmail.com>
2024-04-22 10:49:40 -0300

    vaciamos cache

--- js/app.js
+++ js/app.js
@@ -1,27 +1,3 @@
-if (navigator.serviceWorker) {
-
-    navigator.serviceWorker.getRegistrations().then(function (registrations) {
-        var registrationslength = registrations.length;
-        for (var i = 0; i < registrationslength; i++) {
-            registrations[i].unregister();
-        }
-    }).then(navigator.serviceWorker.register('sw.js'));
-
+if ( navigator.serviceWorker ) {
+    navigator.serviceWorker.register('sw.js');
 }
-
-
-// if ("serviceWorker" in navigator) {
-//     navigator.serviceWorker
-//       .register("/sw.js", { scope: "/" })
-//       .then((registration) => {
-//         // registration worked
-//         console.log("Registration succeeded.");
-//         registration.unregister().then((boolean) => {
-//           // if boolean = true, unregister is successful
-//         });
-//       })
-//       .catch((error) => {
-//         // registration failed
-//         console.error(`Registration failed with ${error}`);
-//       });
-//   }
