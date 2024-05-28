421cbb780df7193b7c034cd77399a7cb29692b83
phpaez <phpaez@gmail.com>
2024-05-28 13:32:47 -0300

    agregamos funcion de limpiar cache

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -2,7 +2,8 @@
 function actualizaCacheDinamico( dynamicCache, req, res ) {
     if ( res.ok ) {
         return caches.open( dynamicCache ).then( cache => {
-            cache.put( req, res.clone() );            
+            cache.put( req, res.clone() );      
+            // limpiarCache( dynamicCache, 5);      
             return res.clone();
         });
     } else {
--- sw.js
+++ sw.js
@@ -7,6 +7,20 @@ const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
+function limpiarCache(cacheName, numeroItems)   {
+    caches.open( cacheName )
+    .then( cache => {
+        return cache.keys()
+            .then( keys => {
+
+                        if (keys.length > numeroItems) {
+                            caches.delete( keys[0] )
+                                .then( limpiarCache( cacheName, numeroItems) );
+                        }
+            });
+    });
+}
+
 const APP_SHELL = [
     // '/',
     'index.html',
