300a4936d6a748acf81adb4946fa0628102f1d38
phpaez <phpaez@gmail.com>
2024-05-28 14:16:56 -0300

    espacio

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('Documento cargado...');
+  
   const applicationServerKey =
     'BNcx2d1PC2OopvBaLgj7Q6CurmEU-R98nE_B6PQmDJ6f6kgn8JfQX45knve7ExUXz_BnlCzU9BMQ9E_Gf-X1tbs';
   let isPushEnabled = false;
