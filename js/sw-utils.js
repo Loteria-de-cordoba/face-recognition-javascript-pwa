0e520efe289c6ffb80e37d2f5a3252ae9acd87fe
phpaez <phpaez@gmail.com>
2024-06-01 20:15:37 -0300

    cambiamos de lugar carga de archivos

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -3,7 +3,7 @@ function actualizaCacheDinamico( dynamicCache, req, res ) {
     if ( res.ok ) {
         return caches.open( dynamicCache ).then( cache => {
             cache.put( req, res.clone() );      
-            // limpiarCache( dynamicCache, 5);      
+            limpiarCache( dynamicCache, 5);      
             return res.clone();
         });
     } else {
--- sw.js
+++ sw.js
@@ -2,9 +2,13 @@
 importScripts('js/sw-utils.js');
 
 
+// 2 - Cache with Network Fallback
+
 const STATIC_CACHE    = 'static-v1';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
+
+const DYNAMIC_CACHE_LIMIT = 50;
 
 
 function limpiarCache(cacheName, numeroItems)   {
@@ -35,8 +39,8 @@ const APP_SHELL = [
 ];
 
 const APP_SHELL_INMUTABLE = [
-    'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',
     'jquery-3.7.1/jquery-3.7.1.min.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',    
     'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js',
     'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js',
     'sweetalert2@11/sweetalert2@11.js',
